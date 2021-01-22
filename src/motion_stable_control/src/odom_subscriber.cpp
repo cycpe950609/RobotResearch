1f9a40c5e3076f08cb9c8e5a2d1ed9b142b0d225
Cycpe950609 <cycpe950609@gmail.com>
2021-01-22 22:43:32 +0800

    Publish new const Twist to 'cmd_vel'
    TODO : Need to calculate new Twist by stable function

--- src/motion_stable_control/src/odom_subscriber.cpp
+++ src/motion_stable_control/src/odom_subscriber.cpp
@@ -4,23 +4,56 @@
 
 #include "ros/ros.h"
 #include "geometry_msgs/Pose.h"
+#include "geometry_msgs/Twist.h"
 #include "nav_msgs/Odometry.h"
 //#include "nav_msgs/Odometry.h"
 #include <cstdio>
 #include <cmath>
 
+#define BURGER_MAX_LIN_VEL  0.22
+#define BURGER_MAX_ANG_VEL  2.84
+
+// 誤差多少以內算是到達目的地
+#define DistanceError 0.2
 #define msg_odom msg
 
 // Function define 
 float EuclideanDistance( float px1, float py1, float px2, float py2 );
 void Go2Goal( float px, float py );
-
+void setTurtlebotTwistCallback(const geometry_msgs::Twist& msg);
+void getPoseCallback(const nav_msgs::Odometry::ConstPtr& msg);
 // ===============
 
+ros::Subscriber odom_subscriber;
+
+ros::Publisher  new_twist;	   
+
+int main(int argc, char **argv)
+{
+    ros::init(argc, argv, "motion_stable_control");
+
+    ros::NodeHandle nh;//Used to communicate with roscore
+
+    odom_subscriber = nh.subscribe("odom",100,getPoseCallback);//TODO: subscribed node's name
+    new_twist	    = nh.advertise<geometry_msgs::Twist>("cmd_vel",1000);
+
+    ros::spin();
+
+    printf("Node is startup");
+
+    return 0;
+}
+
+//Callback when new Twist of Turtlebot updated
+void setTurtlebotTwistCallback(const geometry_msgs::Twist& msg)
+{
+     
+}
 
 //Callback when Pose msg receive
 void getPoseCallback(const nav_msgs::Odometry::ConstPtr& msg)
 {
+    /*
     static int count = 0;
     count++;
     if(count == 10)
@@ -32,21 +65,19 @@ void getPoseCallback(const nav_msgs::Odometry::ConstPtr& msg)
     	ROS_INFO("Orientation-> x: [%f], y: [%f], z: [%f], w: [%f]", msg->pose.pose.orientation.x, msg->pose.pose.orientation.y, msg->pose.pose.orientation.z, msg->pose.pose.orientation.w);
     	ROS_INFO("Vel-> Linear: [%f], Angular: [%f]", msg->twist.twist.linear.x,msg->twist.twist.angular.z);
     }
-}
+    */
+    geometry_msgs::Twist twist;
 
-int main(int argc, char **argv)
-{
-    ros::init(argc, argv, "motion_stable_control");
+    twist.linear.x = BURGER_MAX_LIN_VEL;
+    twist.linear.y = 0.0;
+    twist.linear.z = 0.0;
 
-    ros::NodeHandle nh;//Used to communicate with roscore
+    twist.angular.x = 0.0; 
+    twist.angular.y = 0.0; 
+    twist.angular.z = 0.0;
 
-    ros::Subscriber odom_subscriber = nh.subscribe("odom",100,getPoseCallback);//TODO: subscribed node's name
+    new_twist.publish(twist);
 
-    ros::spin();
-
-    printf("Node is startup");
-
-    return 0;
 }
 
 float EuclideanDistance( float px1, float py1, float px2, float py2 )
